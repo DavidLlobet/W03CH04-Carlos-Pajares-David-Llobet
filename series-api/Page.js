ae42be5bff9c764168c5a90cd1b0233fb9ea2c56
Carlos Pajares <c.pajares@hotmail.com>
2021-10-14 20:28:44 +0200

    fixed New form to prepend

--- series-api/Page.js
+++ series-api/Page.js
@@ -1,3 +1,4 @@
+import Form from "./Form.js";
 class Page {
   section;
 
@@ -12,10 +13,12 @@ class Page {
   createSection() {
     this.section = document.createElement("section");
     this.section.className = "form";
-    document.querySelector(".main").append(this.section);
+    document.querySelector(".main").prepend(this.section);
   }
 
   printForm() {
     new Form(this.section, "form-series", "form");
   }
 }
+
+export default Page;
